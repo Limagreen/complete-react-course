f646462e8743f84c425e8b1f851d6c21fa7f14e6
Limagreen <89492602+Limagreen@users.noreply.github.com>
2022-06-27 13:52:46 -0400

    RP1: Set Title Button
    Made title a stateful property using useState. It can be changed with a button added to ExpenseItem.js
    
    Currently, the button does not allow for dynamically setting the new title value.

--- react-project-1/src/components/expenses/ExpenseItem.js
+++ react-project-1/src/components/expenses/ExpenseItem.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate"
 import Card from "../ui/Card"
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) { // props (an object) scontains all attribute-value (key-value) pairs passed in component-call. By convention it's named "props"
+
+    const [title, setTitle] = useState(props.title); // array destructuring here ([ , ]) lets us use the array elements as two separate const variables, title and setTitle
+
+    function clickHandler() {
+        setTitle('Updated!');
+    }
 
     return (
         <Card className="expense-item">
@@ -10,13 +18,14 @@ function ExpenseItem(props) { // props (an object) scontains all attribute-value
             <ExpenseDate date={props.date} /> {/* There's no closing element tag because I have no inner HTML */}
 
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">
                     ${props.amount}
                 </div> {/* The '$' is literally just to have a $ appear in the text */}
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card > // end of component roots
     );
-}
+} // end of ExpenseItem()
 
 export default ExpenseItem;
