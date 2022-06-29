27283fb2a29f4a148ff627b9901d555b30e5e73a
Limagreen <89492602+Limagreen@users.noreply.github.com>
2022-06-29 10:45:52 -0400

    RP1: Stateless ExpenseItem
    Returned ExpenseItem to statelessness by removing the "update title" button and associated state, along with unneeded comments.

--- react-project-1/src/components/expenses/ExpenseItem.js
+++ react-project-1/src/components/expenses/ExpenseItem.js
@@ -4,27 +4,21 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) { // props (an object) scontains all attribute-value (key-value) pairs passed in component-call. By convention it's named "props"
-
-    const [title, setTitle] = useState(props.title); // array destructuring here ([ , ]) lets us use the array elements as two separate const variables, title and setTitle
-
-    function clickHandler() {
-        setTitle('Updated!');
-    }
+function ExpenseItem(props) {
 
     return (
         <Card className="expense-item">
 
-            <ExpenseDate date={props.date} /> {/* There's no closing element tag because I have no inner HTML */}
+            <ExpenseDate date={props.date} />
 
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">
                     ${props.amount}
-                </div> {/* The '$' is literally just to have a $ appear in the text */}
+                </div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
-        </Card > // end of component roots
+        </Card >
     );
 } // end of ExpenseItem()
 
