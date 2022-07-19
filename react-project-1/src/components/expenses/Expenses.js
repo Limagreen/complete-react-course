694ac43187d8415881766ba696507227c01d9fe0
Limagreen <89492602+Limagreen@users.noreply.github.com>
2022-07-19 13:21:25 -0400

    Option #3
    Re-did it AGAIN to show how components (and function calls) can be stored in variables.

--- react-project-1/src/components/expenses/Expenses.js
+++ react-project-1/src/components/expenses/Expenses.js
@@ -16,22 +16,28 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === year;
     });
 
+    // proof of concept, you can store both pre-made and custom components in variables, along with function calls
+    let expensesContent = <p>No expenses found.</p>; // default message
+
+    // if a non-empty list, change expensesContent
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense =>
+            <ExpenseItem
+                key={expense.id} // add a unique id to the new expense for React to be able to track it
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        );
+    }
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={year} onChangeFilter={changeYearHandler} />
 
-            {/* if filteredExpenses is empty display a default message */}
-            {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+            {/* if filteredExpenses is empty display a default message, else render the list */}
+            {expensesContent}
 
-            {/* if filteredExpenses isn't empty render the list*/}
-            {filteredExpenses.length > 0 && filteredExpenses.map(expense =>
-                <ExpenseItem
-                    key={expense.id} // add a unique id to the new expense for React to be able to track it
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            )}
         </Card>
     );
 }
